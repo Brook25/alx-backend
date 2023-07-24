bbec18be660d5aa075141859569a0143abb742bf
Brook25 <bekelebrook24@gmail.com>
2023-07-24 20:17:12 +0000

    LRU caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -22,7 +22,7 @@ class LRUCache(BaseCaching):
         self.cache_data[key] = item
         if key in self.tracker:
             self.tracker.remove(key)
-        if len(self.cache_data) > self.MAX_ITEMS:
+        if len(self.tracker) == self.MAX_ITEMS:
             discard = self.tracker.pop()
             self.cache_data.pop(discard)
             print('DISCARD: {}'.format(discard))
@@ -31,5 +31,37 @@ class LRUCache(BaseCaching):
     def get(self, key):
         """Returns value associated with key in cache
         """
-        if key:
-            return self.cache_data.get(key)
+        if key and key in self.tracker:
+            self.tracker.remove(key)
+            self.tracker.insert(0, key)
+        return self.cache_data.get(key)
+
+try:
+
+    BaseCaching.MAX_ITEMS = 5
+    LRUCache.MAX_ITEMS = 5
+    my_cache = LRUCache()
+    my_cache.MAX_ITEMS = 5
+    prev_key = None
+
+    for i in range(10):
+        key = "key-{}".format(i)
+        value = "value-{}".format(i)
+        if prev_key is not None:
+            my_cache.get(prev_key)
+        prev_key = key
+        my_cache.put(key, value)
+        my_cache.print_cache()
+    
+    my_cache.get("key-0")
+    my_cache.get("key-4")
+    my_cache.get("key-6")
+    my_cache.get("key-7")
+    my_cache.print_cache()
+    my_cache.put("key-20", "value-20")
+    my_cache.put("key-21", "value-21")
+    my_cache.put("key-22", "value-22")
+    my_cache.print_cache()
+
+except:
+    print(sys.exc_info()[1])
