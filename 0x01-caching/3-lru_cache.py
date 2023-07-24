10ecacee08a0bebd8223c3d0116c306ad051a4c3
Brook25 <bekelebrook24@gmail.com>
2023-07-24 20:18:12 +0000

    LRU caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -35,33 +35,3 @@ class LRUCache(BaseCaching):
             self.tracker.remove(key)
             self.tracker.insert(0, key)
         return self.cache_data.get(key)
-
-try:
-
-    BaseCaching.MAX_ITEMS = 5
-    LRUCache.MAX_ITEMS = 5
-    my_cache = LRUCache()
-    my_cache.MAX_ITEMS = 5
-    prev_key = None
-
-    for i in range(10):
-        key = "key-{}".format(i)
-        value = "value-{}".format(i)
-        if prev_key is not None:
-            my_cache.get(prev_key)
-        prev_key = key
-        my_cache.put(key, value)
-        my_cache.print_cache()
-    
-    my_cache.get("key-0")
-    my_cache.get("key-4")
-    my_cache.get("key-6")
-    my_cache.get("key-7")
-    my_cache.print_cache()
-    my_cache.put("key-20", "value-20")
-    my_cache.put("key-21", "value-21")
-    my_cache.put("key-22", "value-22")
-    my_cache.print_cache()
-
-except:
-    print(sys.exc_info()[1])
