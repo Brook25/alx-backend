44ea444620258657b49fd905e5952d5dbb4b2c6d
Brook25 <bekelebrook24@gmail.com>
2023-07-25 16:10:05 +0000

    MRU caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""MRU  caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU caching system
+    """
+    def __init__(self):
+        """Init cache system
+        """
+        super().__init__()
+        self.discard = None
+
+    def put(self, key, item):
+        """Adds data to caching system
+        """
+        if not (key and item):
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            self.cache_data.pop(self.discard)
+            print('DISCARD: {}'.format(self.discard))
+        if len(self.cache_data) == self.MAX_ITEMS:
+            self.discard = key
+
+    def get(self, key):
+        """Returns value associated with key in cache
+        """
+        if key and key in self.cache_data:
+            self.discard = key
+            return self.cache_data.get(key)
