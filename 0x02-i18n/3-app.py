222a28ce5ee55363cf8918565ed67a85ada22577
Brook25 <bekelebrook24@gmail.com>
2023-08-01 13:28:06 +0000

    3rd task redo

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -7,7 +7,7 @@ from flask_babel import Babel, _
 
 
 class Config(object):
-    """app config class"""
+    """app configuration class"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
